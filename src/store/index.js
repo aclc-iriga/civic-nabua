fcbe2da78000168bc1b919715ae81869df42a0d7
kulotsystems <kulotsystems@gmail.com>
2023-04-30 20:22:18 +0800

    Prepare for Annual Civic Street Parade (Nabua, Camarines Sur).

--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,7 @@ export default createStore({
 
     state: {
         app: {
-            title: 'Binibining San Vicente 2023',
+            title: 'Annual Civic Street Parade 2023',
             backendDir: 'app',
             sideNav: false
         },
@@ -20,8 +20,8 @@ export default createStore({
             height: 0
         },
         rating: {
-            min: 70,
-            max: 100
+            min: 1,
+            max: 50
         },
         deduction: {
             min: 0,
@@ -38,7 +38,7 @@ export default createStore({
         // get app url
         appURL(state) {
             const location = window.location;
-            if(location.hostname === 'localhost' && location.port === '5178')
+            if(location.hostname === 'localhost' && location.port === '5183')
                 return `http://localhost${import.meta.env.BASE_URL}${state.app.backendDir}`;
             else
                 return `${location.protocol}//${location.hostname}${import.meta.env.BASE_URL}${state.app.backendDir}`;
